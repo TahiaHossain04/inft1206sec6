4863b8237bf5270f96023435e8227baa9d799d71
TahiaHossain04 <tahia.hossain@dcmail.ca>
2024-08-01 13:27:08 -0400

    a4p1v3-js
    Copy pasted the rest text part 3

--- assignments/assignment4/a4p1/main.js
+++ assignments/assignment4/a4p1/main.js
@@ -1,3 +1,4 @@
+// 1
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -7,6 +8,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
+// 2
 It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
 Willy the Goblin
@@ -20,3 +22,24 @@ the White House
 spontaneously combusted
 melted into a puddle on the sidewalk
 turned into a slug and crawled away
+
+// 3
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
