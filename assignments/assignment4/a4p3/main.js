83d6ca269166c40f0332b398ab2db97976e8fe7f
TahiaHossain04 <tahia.hossain@dcmail.ca>
2024-08-01 16:31:37 -0400

    a3p4v11
    Fixed the code according to instructions and added some comment to indicate changes

--- assignments/assignment4/a4p3/main.js
+++ assignments/assignment4/a4p3/main.js
@@ -7,6 +7,10 @@ Description  : Raw JavaScript text copy pasted here and did modifications.
 
 // set up canvas
 
+// 1
+const paragraph = document.querySelector('p')
+let count = 0;
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -25,21 +29,23 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
+// 2
 class Shape {
   constructor(x, y, velX, velY) {
-
-  }
-}
-class Ball extends Shape {
-  constructor(x, y, velX, velY, color, size) {
-    super(x, y, velX, velY)
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+// 3
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY)
     this.color = color;
     this.size = size;
-    
+    this.exists = true;
   }
 
   draw() {
@@ -85,6 +91,83 @@ class Ball extends Shape {
   }
 }
 
+// 4
+class EvilCircle extends Shape {
+
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener('keydown', (e) => {
+      switch(e.key)
+      {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    }
+  );
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.lineWidth = 3;
+  }
+
+  checkBounds() {
+    if ((this.x + this.size) >= width)
+      {
+      this.x -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0)
+      {
+      this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height)
+      {
+      this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0)
+      {
+      this.y += this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size)
+          {
+          ball.exists = false;
+          count--;
+          paragraph.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
+
+}
+
 const balls = [];
 
 while (balls.length < 25) {
@@ -100,18 +183,32 @@ while (balls.length < 25) {
     size
   );
 
+  // 5
   balls.push(ball);
+  count++
+  paragraph.textContent = 'Ball Count: ' + count;
 }
 
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+  for (const ball of balls)
+    {
+      if (ball.exists)
+        {
+          // 6
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+      }
   }
+  
+  // 7
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
 
   requestAnimationFrame(loop);
 }
