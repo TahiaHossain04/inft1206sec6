bc001f162708993780d1fe0c5daf117629032400
TahiaHossain04 <tahia.hossain@dcmail.ca>
2024-08-02 14:19:39 -0400

    A4 PART3 - JS 4
    Added the update() to the ball

--- assignments/assignment4/a4part3/main.js
+++ assignments/assignment4/a4part3/main.js
@@ -39,11 +39,36 @@ class Ball {
     this.size = size;
   }
 
+  // 2
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+
+  // 3
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
   
 }
+
